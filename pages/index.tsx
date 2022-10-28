393f4558c8a3d8f67e63390d4a336e6963fe7503
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 16:16:34 +0300

    Roaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
-import {useActiveListings, useContract} from "@thirdweb-dev/react";
+import {MediaRenderer, useActiveListings, useContract} from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
     const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, 'marketplace');
@@ -20,12 +20,30 @@ const Home: NextPage = () => {
 
         <Header />
 
-        <main>
+        <main className="max-w-6xl mx-auto p-2">
             {loadingListings ? (
-                <p>Loading listings...</p>
+                <p className="text-center animate-pulse text-blue-500">Loading listings...</p>
             ) : (
-                <div>
-                    Listings
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
+                    {listings?.map((listing) => (
+                        <div className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out cursor-pointer" key={listing.id}>
+                            <div className="flex-1 flex-col pb-2 items-center">
+                                <MediaRenderer className="w-44" src={listing.asset.image} />
+                            </div>
+                            <div className="pt-2 space-y-4">
+                                <div>
+                                    <h2 className="text-lg truncate">{listing.asset.name}</h2>
+                                    <hr/>
+                                    <p className="truncate text-sm text-gray-600 mt-2">{listing.asset.description || 'Loading...'}</p>
+                                </div>
+
+                                <p>
+                                    <span className="font-bold">{listing.buyoutCurrencyValuePerToken.displayValue}</span>
+                                    {listing.buyoutCurrencyValuePerToken.symbol}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             )}
         </main>
