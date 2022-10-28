b46024120e7a969a7acd27f7ada23d15319db4ec
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 16:05:35 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import {useActiveListings, useContract} from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
+    const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, 'marketplace');
+
+    const { data: listings, isLoading: loadingListings } = useActiveListings(contract);
+    console.log(listings);
+
   return (
     <div className="">
       <Head>
@@ -13,6 +19,16 @@ const Home: NextPage = () => {
 
 
         <Header />
+
+        <main>
+            {loadingListings ? (
+                <p>Loading listings...</p>
+            ) : (
+                <div>
+                    Listings
+                </div>
+            )}
+        </main>
 
     </div>
   )
