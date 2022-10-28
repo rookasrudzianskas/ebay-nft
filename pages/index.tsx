202562e6191745dbd8f4dd2cfd0692c8811547f2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 16:25:08 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
 import {MediaRenderer, useActiveListings, useContract} from "@thirdweb-dev/react";
-
+import {ListingType} from "@thirdweb-dev/sdk";
+import {BellIcon, ShoppingCartIcon, ChevronDownIcon,
+    GlobeIcon
+} from "@heroicons/react/outline";
 const Home: NextPage = () => {
     const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, 'marketplace');
 
@@ -27,7 +30,7 @@ const Home: NextPage = () => {
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
                     {listings?.map((listing) => (
                         <div className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out cursor-pointer" key={listing.id}>
-                            <div className="flex-1 flex-col pb-2 items-center">
+                            <div className="flex-1 flex flex-col pb-2 items-center">
                                 <MediaRenderer className="w-44" src={listing.asset.image} />
                             </div>
                             <div className="pt-2 space-y-4">
@@ -37,10 +40,20 @@ const Home: NextPage = () => {
                                     <p className="truncate text-sm text-gray-600 mt-2">{listing.asset.description || 'Loading...'}</p>
                                 </div>
 
-                                <p>
+                                <p className="">
                                     <span className="font-bold">{listing.buyoutCurrencyValuePerToken.displayValue}</span>
-                                    {listing.buyoutCurrencyValuePerToken.symbol}
+                                    {" "}{listing.buyoutCurrencyValuePerToken.symbol}
                                 </p>
+                                <div className={`flex items-center space-x-1 justify-end text-xs border w-fit ml-auto p-2 rounded-lg text-white ${listing.type === ListingType.Direct ? "bg-blue-500" : "bg-red-500"}`}>
+                                    <p>
+                                        {listing.type === ListingType.Direct ? "Buy Now" : "Auction"}
+                                    </p>
+                                    {listing.type === ListingType.Direct ? (
+                                        <ShoppingCartIcon className="h-4" />
+                                    ) : (
+                                        <GlobeIcon className="h-4" />
+                                        )}
+                                </div>
                             </div>
                         </div>
                     ))}
