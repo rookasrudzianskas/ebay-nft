65144c4559cc074abfc42ddf9a72f1f3c889a165
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 14:18:18 +0300

    Rokas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import network from "../utils/network";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+      <ThirdwebProvider desiredChainId={network}>
         <Component {...pageProps} />
       </ThirdwebProvider>
   )
