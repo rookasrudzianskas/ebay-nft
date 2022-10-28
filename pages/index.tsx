3d4481a75e4ca89ba214f1dc56972093bd94bb3c
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 14:09:45 +0300

    Rokas

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Ebay</title>
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/5977/5977581.png" />
+      </Head>
+
+
+    </div>
+  )
+}
+
+export default Home
