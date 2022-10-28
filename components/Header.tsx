cce2dff40c007457ff859af54618a7302d591476
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 14:21:20 +0300

    Rokas

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+
+}
+
+const Header = ({}: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
+// by Rokas with ❤️
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/5977/5977581.png" />
       </Head>
 
+
+        <Header />
 
     </div>
   )
