bf3df1a7726412c7428d5c51d70252db0b722dba
Rokas <rookas.rudzenskas@gmail.com>
2022-10-29 18:56:56 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {ListingType} from "@thirdweb-dev/sdk";
 import {BellIcon, ShoppingCartIcon, ChevronDownIcon,
     GlobeIcon
 } from "@heroicons/react/outline";
+import Link from "next/link";
 const Home: NextPage = () => {
     const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, 'marketplace');
 
@@ -29,33 +30,35 @@ const Home: NextPage = () => {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
                     {listings?.map((listing) => (
-                        <div className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out cursor-pointer" key={listing.id}>
-                            <div className="flex-1 flex flex-col pb-2 items-center">
-                                <MediaRenderer className="w-44" src={listing.asset.image} />
-                            </div>
-                            <div className="pt-2 space-y-4">
-                                <div>
-                                    <h2 className="text-lg truncate">{listing.asset.name}</h2>
-                                    <hr/>
-                                    <p className="truncate text-sm text-gray-600 mt-2">{listing.asset.description || 'Loading...'}</p>
+                        <Link href={`/listing${listing.id}`}>
+                            <div className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out cursor-pointer" key={listing.id}>
+                                <div className="flex-1 flex flex-col pb-2 items-center">
+                                    <MediaRenderer className="w-44" src={listing.asset.image} />
                                 </div>
+                                <div className="pt-2 space-y-4">
+                                    <div>
+                                        <h2 className="text-lg truncate">{listing.asset.name}</h2>
+                                        <hr/>
+                                        <p className="truncate text-sm text-gray-600 mt-2">{listing.asset.description || 'Loading...'}</p>
+                                    </div>
 
-                                <p className="">
-                                    <span className="font-bold">{listing.buyoutCurrencyValuePerToken.displayValue}</span>
-                                    {" "}{listing.buyoutCurrencyValuePerToken.symbol}
-                                </p>
-                                <div className={`flex items-center space-x-1 justify-end text-xs border w-fit ml-auto p-2 rounded-lg text-white ${listing.type === ListingType.Direct ? "bg-blue-500" : "bg-red-500"}`}>
-                                    <p>
-                                        {listing.type === ListingType.Direct ? "Buy Now" : "Auction"}
+                                    <p className="">
+                                        <span className="font-bold">{listing.buyoutCurrencyValuePerToken.displayValue}</span>
+                                        {" "}{listing.buyoutCurrencyValuePerToken.symbol}
                                     </p>
-                                    {listing.type === ListingType.Direct ? (
-                                        <ShoppingCartIcon className="h-4" />
-                                    ) : (
-                                        <GlobeIcon className="h-4" />
+                                    <div className={`flex items-center space-x-1 justify-end text-xs border w-fit ml-auto p-2 rounded-lg text-white ${listing.type === ListingType.Direct ? "bg-blue-500" : "bg-red-500"}`}>
+                                        <p>
+                                            {listing.type === ListingType.Direct ? "Buy Now" : "Auction"}
+                                        </p>
+                                        {listing.type === ListingType.Direct ? (
+                                            <ShoppingCartIcon className="h-4" />
+                                        ) : (
+                                            <GlobeIcon className="h-4" />
                                         )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Link>
                     ))}
                 </div>
             )}
