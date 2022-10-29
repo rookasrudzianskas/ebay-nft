af0e507a3c045e26fd4ff60622928cc0be45f8b2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-29 19:07:54 +0300

    Rokas

--- pages/listing/[listingId].tsx
+++ pages/listing/[listingId].tsx
@@ -25,7 +25,7 @@ const ListingId = ({}) => {
         <div>
             <Header />
 
-            <main className="max-w-6xl mx-auto p-2 flex-row space-y-10 space-x-5 pr-10">
+            <main className="max-w-6xl mx-auto p-2 flex-col lg:flex-row space-y-10 space-x-5 pr-10">
                 <div className="p-10 border mx-auto lg:mx-0 max-w-md lg:max-w-xl">
                     <MediaRenderer src={listing.asset.image} />
                 </div>
