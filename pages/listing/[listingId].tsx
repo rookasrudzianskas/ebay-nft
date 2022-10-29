069caecfef61ce8078af7eb931beb49d27768cab
Rokas <rookas.rudzenskas@gmail.com>
2022-10-29 19:03:46 +0300

    Rokas

--- pages/listing/[listingId].tsx
+++ pages/listing/[listingId].tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import {useRouter} from "next/router";
+import Header from "../../components/Header";
+import {MediaRenderer, useContract, useListing} from "@thirdweb-dev/react";
 
 const ListingId = ({}) => {
     const router = useRouter();
     const {listingId} = router.query as { listingId: string };
+    const { contract}  = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, 'marketplace');
+    const {data: listing, isLoading, error } = useListing(contract, listingId);
+
+    if(isLoading) return (
+        <div>
+            <Header />
+            <div>
+                <p>Loading Item...</p>
+            </div>
+        </div>
+    )
+
+    if(!listing) {
+        return <p>Listing is not found</p>
+    }
     return (
         <div>
-            <h1>Listing ID: {listingId}</h1>
+            <Header />
+
+            <main>
+                <div>
+                    <MediaRenderer src={listing.asset.image} />
+                </div>
+            </main>
         </div>
     );
 };
